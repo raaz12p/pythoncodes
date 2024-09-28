e2e67f82dde2aaa96c110619c4477986f86ace88
swarajdev <swarajdev@gmail.com>
2024-09-28 16:56:39 +0530

    update

--- .idea/nestedifAge.py
+++
@@ -1,9 +0,0 @@
-age = int (input("Please enter your age "))
-if age > 18 :
-    print("You are an Adult !!")
-elif age < 12:
-    print ("You are a Child")
-elif age >= 12:
-    print ("You are a Teenage")
-else :
-    print ("Please enter your age ")
--- .idea/speed.py
+++
@@ -1,5 +0,0 @@
-speed =int (input())
-if speed > 50 :
-    print ("Please slow down")
-else:
-    print("Happy Journey !!")
